1732c6022e72cbfaa4c36cee5b01877868b98ec8
alfanmahdi <amarthchannel@gmail.com>
2024-10-15 23:51:43 +0700

    feat: fix tailwind

--- resources/views/song.blade.php
+++ resources/views/song.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Ini adalah halaman Home Page</h3>
+    <h3 class="text-xl">Ini adalah halaman Song</h3>
 </x-layout>
